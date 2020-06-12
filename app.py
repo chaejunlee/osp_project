9d0d0efd95589785e99950009c251e0677ab8dba
Chaejun Lee <chaejunlee@knu.ac.kr>
2020-06-13 07:42:28 +0900

    modified main.html & text.html

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-import pandas as pd 
+import pandas as pd
 import re
 import requests
 import sys
@@ -21,13 +21,14 @@ app = Flask(__name__)
 
 host = '127.0.0.1'
 port = '8000'
-es_port = "9200"   
-es = Elasticsearch([{'host':host, 'port':es_port}], timeout=30)
+es_port = "9200"
+es = Elasticsearch([{'host': host, 'port': es_port}], timeout=30)
 swlist = []
 
 index_list = []
 
 word_d = {}
+
 
 def compute_idf(dictionary, sent_list):
     Dval = len(sent_list)
@@ -35,13 +36,14 @@ def compute_idf(dictionary, sent_list):
     idf_d = {}
     for t in dictionary:
         cnt = 0
-        for s in sent_list: 
-            if t in word_tokenize(s): # 문장을 자르는 방법은 word_tokenize()
+        for s in sent_list:
+            if t in word_tokenize(s):  # 문장을 자르는 방법은 word_tokenize()
                 cnt += 1
             if cnt:
                 idf_d[t] = math.log(Dval / cnt)
 
     return idf_d
+
 
 def compute_tf(dictionary, s):
     tf_d = {}
@@ -51,20 +53,25 @@ def compute_tf(dictionary, s):
 
     return tf_d
 
+
 def findDict(sent_list):
     dictionary = {}
     for sentence in sent_list:
-            textSlice = word_tokenize(sentence)
-            for word in textSlice:
-                if word:
-                    if word not in swlist:
-                        try: dictionary[word] += 1
-                        except: dictionary[word] = 1
+        textSlice = word_tokenize(sentence)
+        for word in textSlice:
+            if word:
+                if word not in swlist:
+                    try:
+                        dictionary[word] += 1
+                    except:
+                        dictionary[word] = 1
     return dictionary
 
 # tfidf: 값이 저장 되는 곳
 # dictionary: 유의미한 단어의 딕셔너리
 # sent_list: 유의미한 문장의 리스트
+
+
 def top10Analyze(sent_list):
     dictionary = {}
     tfidf = {}
@@ -77,10 +84,10 @@ def top10Analyze(sent_list):
         tf_d = compute_tf(dictionary, sent_list[i])
         for word, tfval in tf_d.items():
             tfidf[word] = tfval * idf_d[word]
-            
+
     res = sorted(tfidf.items(), key=(lambda x: x[1]), reverse=True)
     top10 = dict(res[:10])  # top10을 뽑음
-    
+
     return top10
 
 
@@ -107,6 +114,8 @@ def insertDoc(idx, url, dictionary, docType):  # ElasticSearch에 저장
 # allWords: 총 단어가 저장되는 리스트
 # dictionary: 유의한 단어를 저장할 리스트
 # sent_list: 유의미(문장부호 등이 없는)한 문장이 저장되어 있는 리스트
+
+
 def findWords(allWords, sent_list):
     totalWordCount = 0
     for sentence in sent_list:
@@ -114,8 +123,10 @@ def findWords(allWords, sent_list):
         for word in textSlice:
             if word:
                 totalWordCount += 1
-                try: allWords[word] += 1
-                except: allWords[word] = 1
+                try:
+                    allWords[word] += 1
+                except:
+                    allWords[word] = 1
 
                 if word not in word_d.keys():
                     word_d[word] = 0
@@ -124,6 +135,8 @@ def findWords(allWords, sent_list):
 
 # html_body: rough하게 크롤링한 데이터가 저장되어 있는 string
 # sent_list: html_body로부터 유의미한(문장부호등이 없는) 문장이 저장될 리스트
+
+
 def findSentList(html_body, sent_list):
     for sentence in html_body:
         body = sentence.text
@@ -134,6 +147,7 @@ def findSentList(html_body, sent_list):
                 cleanWord = re.sub('[^0-9a-zA-Z]', ' ', send)
                 if cleanWord:
                     sent_list.append(cleanWord)
+
 
 def make_vector(sent_list):
     v = []
@@ -146,6 +160,7 @@ def make_vector(sent_list):
                 val += 1
         v.append(val)
     return v
+
 
 def webcrawl(url):
     try:
@@ -164,17 +179,18 @@ def webcrawl(url):
     index_list.append(index)
     print(index_list)
 
-    allWords = {} # 사이트에 있는 모든 글자
-    html_body = [] # 크롤링할 내용이 저장될 리스트
-    sent_list = [] # 크롤링한 내용의 문장들이 저장되는 리스트
+    allWords = {}  # 사이트에 있는 모든 글자
+    html_body = []  # 크롤링할 내용이 저장될 리스트
+    sent_list = []  # 크롤링한 내용의 문장들이 저장되는 리스트
     totalWordCount = 0
 
     elapsedTime = 0
 
     start = time.time()  # 시간 측정 시작
     html_body += html.select('body')  # 웹 페이지 전부 크롤링
-    findSentList(html_body, sent_list) # 크롤링한 데이터를 문장 단위로 추출
-    totalWordCount = findWords(allWords, sent_list)  # 문장 단위로 추출한 데이터를 단어 단위로 추출
+    findSentList(html_body, sent_list)  # 크롤링한 데이터를 문장 단위로 추출
+    # 문장 단위로 추출한 데이터를 단어 단위로 추출
+    totalWordCount = findWords(allWords, sent_list)
     # allWords는 전체 단어의 갯수, dictionary는 유의한 단어의 갯수
 
     insertDoc(index, link, sent_list, 'sent_list')
@@ -183,9 +199,11 @@ def webcrawl(url):
 
     return index, elapsedTime, totalWordCount
 
+
 @app.route('/')
 def render_file():
-    return render_template('home.html')
+    return render_template('main.html')
+
 
 @app.route('/cossimil', methods=['GET', 'POST'])
 def cossimilweb():
@@ -207,7 +225,8 @@ def cossimilweb():
     top3 = []
     url = ''
 
-    result = es.search(index=index, body={'query': {'match': {'type': 'sent_list'}}})
+    result = es.search(index=index, body={
+                       'query': {'match': {'type': 'sent_list'}}})
 
     for data in result['hits']['hits']:
         original_list = (data['_source'].get('words'))
@@ -216,12 +235,12 @@ def cossimilweb():
 
     for idx in index_list:
         if idx != index:
-            result = es.search(index=idx, body={'query': {'match': {'type': 'sent_list'}}})
+            result = es.search(
+                index=idx, body={'query': {'match': {'type': 'sent_list'}}})
 
             for data in result['hits']['hits']:
                 sent_list = (data['_source'].get('words'))
                 url = (data['_source'].get('url'))
-
 
             v2 = make_vector(sent_list)
 
@@ -241,22 +260,24 @@ def cossimilweb():
             'word': word,
         }]
         i += 1
-    
+
     return render_template('cossimil.html', posts=posts)
 
-@app.route('/top10', methods = ['GET', 'POST'])
+
+@app.route('/top10', methods=['GET', 'POST'])
 def top10():
     if request.method == 'POST':
         index = request.form['tfidf']
 
-    result = es.search(index=index, body={'query': {'match': {'type': 'sent_list'}}})
+    result = es.search(index=index, body={
+                       'query': {'match': {'type': 'sent_list'}}})
 
     sent_list = []
     posts = []
 
     for data in result['hits']['hits']:
         sent_list = (data['_source'].get('words'))
-    
+
     top = top10Analyze(sent_list)
 
     i = 1
@@ -266,17 +287,25 @@ def top10():
             'word': word,
         }]
         i += 1
-    
+
     return render_template('top10.html', posts=posts)
 
-@app.route('/file_uploaded', methods = ['GET', 'POST'])
+
+@app.route('/file_uploaded', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        f = request.files['file1']
-        txt = request.form['text1']
+        try:
+            f = request.files['file1']
+        except:
+            f = None
+        try:
+            txt = request.form['text1']
+        except:
+            txt = None
+
         posts = []
         url_list = []
-        if f: # 파일로 받으면 여기로
+        if f:  # 파일로 받으면 여기로
             txt = f.read()
             urls = txt.splitlines()
             for binUrl in urls:
@@ -298,7 +327,7 @@ def upload_file():
                         'url': url,
                     }]
                     url_list.append(url)
-            return render_template('result.html', posts = posts)
+            return render_template('result.html', posts=posts)
 
         if txt:  # 텍스트 하나만 받으면 여기로
             url = txt
@@ -306,18 +335,19 @@ def upload_file():
             index, elapsedTime, totalWordCount = webcrawl(url)
 
             posts += [{
-                    'elapsedTime': elapsedTime,
-                    'totalWordCount': totalWordCount,
-                    'index': index,
-                    'url': url,
-                }]
+                'elapsedTime': elapsedTime,
+                'totalWordCount': totalWordCount,
+                'index': index,
+                'url': url,
+            }]
 
             # 리턴하는 값들: 처리시간(elapsedTime), 전체 단어수(totalWordCount), 쿼리 접근을 위한 인덱스(index)
             return render_template('result.html', posts=posts)
 
+
 if __name__ == '__main__':
-    # debug를 True로 세팅하면, 해당 서버 세팅 후에 코드가 바뀌어도 문제없이 실행됨. 
+    # debug를 True로 세팅하면, 해당 서버 세팅 후에 코드가 바뀌어도 문제없이 실행됨.
     for sw in stopwords.words("english"):
         swlist.append(sw)
-    
+
     app.run(host, port, debug=True)
